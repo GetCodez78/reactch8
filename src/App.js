0caa5efb9e48577ff16431efdf594da99e1eff59
GetCodez78 <ziheng.li@gmail.com>
2017-08-17 21:55:46 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,52 +1,119 @@
 import React, { Component } from 'react';
-import { Appbar, Container } from 'muicss/react';
+import axios from 'axios';
+import {Appbar, Container, Button} from 'muicss/react';
 import Tasks from './Components/Tasks';
-import axios from 'axios'
+import AddTask from './Components/AddTask';
 import './App.css';
 
 class App extends Component {
-  constructor() {  // create the object without a param
-    super()
+  constructor(){
+    super();
     this.state = {
-      tasks: []
+      tasks:[]
     }
   }
 
-  componentWillMount() {
+  componentWillMount(){
     this.getTasks();
   }
 
-  getTasks() {
+  getTasks(){
     axios.request({
       method:'get',
       url: 'https://api.mlab.com/api/1/databases/mytasklist_zeeli/collections/tasks?apiKey=VQthB7cDuT3pjoMXwMPb0p5wXgR3jL4O'
     }).then((response) => {
       this.setState({tasks: response.data}, () => {
         console.log(this.state);
-      })
-    }).catch((error)=> {
+      });
+    }).catch((error) => {
       console.log(error);
-    })
+    });
   }
+
+  editState(task, checked){
+    axios.request({
+      method:'put',
+      url:'https://api.mlab.com/api/1/databases/mytasklist_zeeli/collections/tasks/'+task._id.$oid+'?apiKey=VQthB7cDuT3pjoMXwMPb0p5wXgR3jL4O',
+      data: {
+        text: task.text,
+        completed: checked
+      }
+    }).then((response) => {
+      let tasks = this.state.tasks;
+      for(let i = 0;i < tasks.length;i++){
+        if(tasks[i]._id.$oid === response.data._id.$oid){
+          tasks[i].completed = checked;
+        }
+      }
+      this.setState({tasks: tasks});
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  addTask(text){
+    axios.request({
+      method:'post',
+      url:'https://api.mlab.com/api/1/databases/mytasklist_zeeli/collections/tasks?apiKey=VQthB7cDuT3pjoMXwMPb0p5wXgR3jL4O',
+      data: {
+        text: text,
+        completed: false
+      }
+    }).then((response) => {
+      let tasks = this.state.tasks;
+      tasks.push({
+        _id: response.data._id,
+        text: text,
+        completed: false
+      });
+      this.setState({tasks: tasks});
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+
+  clearTasks(){
+    let tasks = this.state.tasks;
+    let i = tasks.length;
+
+    while(i--){
+      if(tasks[i].completed === true){
+        let id = tasks[i]._id.$oid;
+        tasks.splice(i, 1);
+        axios.request({
+          method:'delete',
+          url:'https://api.mlab.com/api/1/databases/mytasklist_zeeli/collections/tasks/'+id+'?apiKey=VQthB7cDuT3pjoMXwMPb0p5wXgR3jL4O'
+        }).then((response) => {
+
+        }).catch((error) => {
+          console.log(error);
+        });
+      }
+    }
+
+    this.setState({tasks: tasks});
+  }
+
   render() {
-    
     return (
       <div className="App">
         <Appbar>
           <Container>
-        <table width="100%">
-         <tbody>
-           <tr>
-             <td className="mui--appbar-height"><h3>React Tasks</h3></td>
-           </tr>
-         </tbody>
-       </table>
-       </Container>
+          <table width="100%">
+           <tbody>
+             <tr>
+               <td className="mui--appbar-height"><h3>ReactTasks</h3></td>
+             </tr>
+           </tbody>
+         </table>
+         </Container>
         </Appbar>
-        <br/>
+        <br />
         <Container>
-          <Tasks tasks={this.state.tasks}/>
-          </Container>
+          <AddTask onAddTask={this.addTask.bind(this)} />
+          <Tasks onEditState={this.editState.bind(this)} tasks={this.state.tasks} />
+          <Button color="danger" onClick={this.clearTasks.bind(this)}>Clear Tasks</Button>
+        </Container>
       </div>
     );
   }
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,26 +1,27 @@
 import React, { Component } from 'react';
-import { Panel } from 'muicss/react';
+import {Panel} from 'muicss/react';
 import TaskItem from './TaskItem';
 
 class Tasks extends Component {
+  handleEditState(task, checked){
+    this.props.onEditState(task, checked);
+  }
 
-    render() {
-
-        let TaskItems;
-        if (this.props.tasks) {
-            TaskItems = this.props.tasks.map(task => {
-                return (
-                    <TaskItem key={task._id.$oid} task={task} />
-                );
-            })
-        }
-
+  render() {
+    let taskItems;
+    if(this.props.tasks){
+      taskItems = this.props.tasks.map(task => {
         return (
-            <Panel>
-                {TaskItems}
-            </Panel>
+          <TaskItem onEditState={this.handleEditState.bind(this)} key={task._id.$oid} task={task} />
         );
+      });
     }
+    return (
+      <Panel>
+        {taskItems}
+      </Panel>
+    );
+  }
 }
 
 export default Tasks;
