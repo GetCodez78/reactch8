5ec252670004c519de9f413d45c6e67679f819ba
GetCodez78 <ziheng.li@gmail.com>
2017-09-29 23:51:52 -0400

    prettify

--- src/Components/TaskItem.js
+++ src/Components/TaskItem.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
-import {Checkbox} from 'muicss/react';
+import { Checkbox } from 'muicss/react';
 
 class TaskItem extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       task: props.task   // task is props.task or their values are equal, not the left is equal the left (this is the confusion)
     }
   }
 
-  onChange(task, e){
+  onChange(task, e) {
     this.props.onEditState(task, e.target.checked);
   }
 
   render() {
     return (
       <div className="mui--divider-bottom">
-        <Checkbox className={(this.state.task.completed) ? "completed" : ""} onChange={this.onChange.bind(this, this.state.task)} name={this.state.task._id.$oid} label={this.state.task.text} defaultChecked={this.state.task.completed} />
+        <Checkbox className={(this.state.task.completed) ? "completed" : ""}
+          onChange={this.onChange.bind(this, this.state.task)} name={this.state.task._id.$oid}
+          label={this.state.task.text} defaultChecked={this.state.task.completed} />
       </div>
     );
   }
